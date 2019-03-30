0056b1333d69ef3d684ebed36afa6807de45cd91
Majd Saleh <mjd.s.m@hotmail.com>
2019-03-30 12:50:31 +0300

    Game Component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = () => <div>HOME</div>;
+class Home extends Component {
+  
+
+  render() {
+    return (
+      <div className="pt-5 mt-5">
+        <h1>HOME</h1>
+     <button type="submit" className="btn btn-primary" onClick={() => this.props.changeActivePage("game")}>Game</button>
+      </div>
+    );
+  }
+}
 
 export default Home;
