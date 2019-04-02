e8e6ac1e032364d47bd02183513896db21e61277
fajrsaleh1 <fajrsalbakiri@gmail.com>
2019-04-02 13:53:56 +0300

    Added pages for levels and buttons

--- src/components/GameStuff/GameEasy.js
+++ src/components/GameStuff/GameEasy.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import GameType from "./GameType";
 
-class Game extends Component {
+class GameEasy extends Component {
 
 
   render() {
     return (
       <div className="pt-5 mt-5">
-        <GameType changeActivePage={this.props.changeActivePage} />
+        <h1>HI easy</h1>
       </div>
     );
   }
 }
 
-export default Game;
+export default GameEasy;
---
+++ src/components/GameStuff/GameMed.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import GameType from "./GameType";
+
+class GameMed extends Component {
+
+
+    render() {
+        return (
+            <div className="pt-5 mt-5">
+                <h1>HI med</h1>
+            </div>
+        );
+    }
+}
+
+export default GameMed;
