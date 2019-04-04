89d3e258d7537c4afbf81c98704673f25bc7948f
“Majd_saleh” <mjd.s.m@hotmail.com>
2019-04-04 10:15:07 +0300

    Updates

---
+++ precache-manifest.b082bb70972643c796352cac45e7f000.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3fbd769d465037a8f8c",
+    "url": "/front-end/static/css/main.d20b33e2.chunk.css"
+  },
+  {
+    "revision": "e3fbd769d465037a8f8c",
+    "url": "/front-end/static/js/main.53a717d2.chunk.js"
+  },
+  {
+    "revision": "826b249d99d5bcede270",
+    "url": "/front-end/static/js/runtime~main.d65dd991.js"
+  },
+  {
+    "revision": "448d37f6930e905eb655",
+    "url": "/front-end/static/css/2.8b0e0a1d.chunk.css"
+  },
+  {
+    "revision": "448d37f6930e905eb655",
+    "url": "/front-end/static/js/2.011afce2.chunk.js"
+  },
+  {
+    "revision": "5f3db58d5d73d58f2d601dfa6fd7739a",
+    "url": "/front-end/static/media/login.5f3db58d.png"
+  },
+  {
+    "revision": "37a5a87605fb385bcfc0fa04db9e0265",
+    "url": "/front-end/static/media/signup.37a5a876.png"
+  },
+  {
+    "revision": "7e43c91b62010abd39fd0acf19786c65",
+    "url": "/front-end/static/media/trophy.7e43c91b.png"
+  },
+  {
+    "revision": "ad33a2b605d7b74a2269d589639d3c33",
+    "url": "/front-end/static/media/gamesound.ad33a2b6.mp3"
+  },
+  {
+    "revision": "0f76e50057d19d1d680a19919ad133e5",
+    "url": "/front-end/static/media/sand.0f76e500.png"
+  },
+  {
+    "revision": "865218714303a3b6a04cf03beea48f77",
+    "url": "/front-end/static/media/loginPage.86521871.svg"
+  },
+  {
+    "revision": "a410b6368d627d7fe183db9cf5e2f643",
+    "url": "/front-end/index.html"
+  }
+];
